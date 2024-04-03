07ae620f14beafebe2b1fdb698ec2c6b0afdc44f
TJ Crouse <132241035+TJ-Crouse1025@users.noreply.github.com>
2024-04-03 18:31:24 -0400

    player attack

--- NoLF/Assets/Scripts/Movement.cs
+++ NoLF/Assets/Scripts/Movement.cs
@@ -5,11 +5,7 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public float speed = 5f; // Adjust the speed as needed
-    public GameObject attackHitboxPrefab; // Reference to the attack hitbox prefab
-    public Transform attackPoint; // Point where the attack hitbox will be spawned
-    public float attackCooldown = 1f; // Time between attacks
     private Rigidbody2D rb;
-    private float nextAttackTime = 0f;
 
     void Start()
     {
@@ -30,19 +26,5 @@ public class Movement : MonoBehaviour
 
         // Move the character
         rb.velocity = new Vector2(movement.x * speed, movement.y * speed);
-
-        // Attack input handling
-        if (Input.GetKeyDown(KeyCode.Space) && Time.time >= nextAttackTime)
-        {
-            // attackPoint = transform.position;
-            Attack();
-            nextAttackTime = Time.time + 1f / attackCooldown;
-        }
-    }
-
-    void Attack()
-    {
-        // Spawn the attack hitbox in front of the character
-        Instantiate(attackHitboxPrefab, attackPoint.position, attackPoint.rotation);
     }
 }
---
+++ NoLF/Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private Transform attackTransform;
+    private float attackRange = 1.5f;
+    private LayerMask attackableLayer;
+    private float nextAttackTime;
+    private float attackCooldown = 1f;
+
+    private RaycastHit2D[] hits;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Attack input handling
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time >= nextAttackTime)
+        {
+            // attackPoint = transform.position;
+            Attack();
+            nextAttackTime = Time.time + 1f / attackCooldown;
+        }
+    }
+
+    void Attack()
+    {
+        hits = Physics2D.CircleCastAll(attackTransform.position, attackRange, transform.right, 0f, attackableLayer);
+    }
+}
