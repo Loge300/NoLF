ac6055c0e5cafe85f168d66167b8181da27cb8e9
Jamcat42 <jamcatjunk@outlook.com>
2024-05-05 12:00:40 -0400

    initial wave spawner
    Still needs visualization added.  Such as the ability to see when the next wave will spawn and which wave it is currently

--- NoLF/Assets/Resources/Scripts/EnemySpawner.cs
+++ NoLF/Assets/Resources/Scripts/EnemySpawner.cs
@@ -5,10 +5,21 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField]
-    private float spawnRadius = 7, time = 1.5f;
+    private float spawnRadius = 7;
+    private bool isWaveActive = true;
+    private bool stopSpawning = false;
     [SerializeField] private Vector2 _targetPosition;
 
     public GameObject[] enemies;
+
+    private int waveCount;
+    private int enemyCount;
+    private float nextWaveTime = 1.0f;
+    private float spawnRate = 1.0f;
+    private float timeBetweenWaves = 5.0f;
+
+    
+
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +27,7 @@ public class EnemySpawner : MonoBehaviour
         //Sets the spawner center to center
         _targetPosition = Vector2.zero;
 
-        StartCoroutine(SpawnAnEnemy());
+        StartCoroutine(spawnAWave());
     }
 
     // Update is called once per frame
@@ -26,13 +37,35 @@ public class EnemySpawner : MonoBehaviour
 
     }
 
-    IEnumerator SpawnAnEnemy() 
+    IEnumerator spawnAWave() 
     {
-        Vector2 spawnPos = _targetPosition;
-        spawnPos += Random.insideUnitCircle.normalized * spawnRadius;
+        while (isWaveActive == true && stopSpawning == false) 
+        {
+            //setting of spawn position and selection of which enemy will be spawned
+            Vector2 spawnPos = _targetPosition;
+            spawnPos += Random.insideUnitCircle.normalized * spawnRadius;
+            int randomEnemy = Random.Range(0, enemies.Length);
+            isWaveActive = false;
 
-        Instantiate(enemies[Random.Range(0, enemies.Length)], spawnPos, Quaternion.identity);
-        yield return new WaitForSeconds(time);
-        StartCoroutine(SpawnAnEnemy());
+            //spawns enemies one at a time based on the spawn rate but waits to do so unitl the wave timer is complete
+            for (int i = 0; i < enemyCount; i++) 
+            {
+                //ActivateWaveText();
+                yield return new WaitForSeconds(nextWaveTime);
+                //wave
+
+                Instantiate(enemies[Random.Range(0, enemies.Length)], spawnPos, Quaternion.identity);
+                yield return new WaitForSeconds(spawnRate);
+            }
+
+            //Makes the next wave harder and resets other variables needed.  
+            spawnRate -= 0.2f;
+            enemyCount += 1;
+            yield return new WaitForSeconds(timeBetweenWaves);
+            waveCount += 1;
+            isWaveActive = true;
+        }
+
+        
     }
 }
