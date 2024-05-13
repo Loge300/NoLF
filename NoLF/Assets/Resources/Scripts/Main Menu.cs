8b38a18a2076c0633052c2739ef5c278bc12c9b6
Casey Topp <toppcm@miamioh.edu>
2024-05-13 15:55:30 -0400

    info menu

--- NoLF/Assets/Resources/Scripts/Main Menu.cs
+++ NoLF/Assets/Resources/Scripts/Main Menu.cs
@@ -8,10 +8,30 @@ using UnityEngine.UI;
 public class MainMenu : MonoBehaviour
 {
 
+    [SerializeField] public GameObject[] menuObjects;
+    [SerializeField] public GameObject[] infoObjects;
 
     public void PlayGame()
     {
         SceneManager.LoadScene("UISceneCopy");
+    }
+
+    public void showInfo() {
+        for (int i = 0; i < menuObjects.Length; i++) {
+            menuObjects[i].SetActive(false);
+        }
+        for (int i = 0; i < infoObjects.Length; i++) {
+            infoObjects[i].SetActive(true);
+        }
+    }
+
+    public void hideInfo() {
+        for (int i = 0; i < menuObjects.Length; i++) {
+            menuObjects[i].SetActive(true);
+        }
+        for (int i = 0; i < infoObjects.Length; i++) {
+            infoObjects[i].SetActive(false);
+        }
     }
 
     public void Exit()
